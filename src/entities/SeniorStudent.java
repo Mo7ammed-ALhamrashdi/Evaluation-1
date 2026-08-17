1529f1387cb421cc22e5a780265cba299816222d
MOHAMMED ALHAMRASHDI <Malhamrashdi@gmail.com>
2026-08-17 14:24:05 +0400

    SeniorStudent

--- src/entities/SeniorStudent.java
+++ src/entities/SeniorStudent.java
@@ -1,4 +1,24 @@
 package entities;
 
-public class SeniorStudent {
+public class SeniorStudent extends Student {
+
+    private String major;
+    private double gpa;
+
+    public SeniorStudent(String id, String firstName, String lastName,
+                         String dateOfBirth, String gender,
+                         String phoneNumber, String email, String address,
+                         String gradeLevel, String enrollmentDate,
+                         double feeBalance,
+                         String major, double gpa) {
+
+        super(id, firstName, lastName,
+                dateOfBirth, gender,
+                phoneNumber, email, address,
+                gradeLevel, enrollmentDate,
+                feeBalance);
+
+        this.major = major;
+        this.gpa = gpa;
+    }
 }
