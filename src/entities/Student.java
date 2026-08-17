8972da53dfb13c3142f40caa807b35d5fe5ca610
MOHAMMED ALHAMRASHDI <Malhamrashdi@gmail.com>
2026-08-17 12:10:21 +0400

    Student

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,6 +1,10 @@
 package entities;
+
 import java.util.ArrayList;
 import java.util.List;
-public class Student {
+public class Student extends Person {
 
-}
+    private String gradeLevel;
+    private String enrollmentDate;
+    private double feeBalance;
+    private List<String> subjects;
