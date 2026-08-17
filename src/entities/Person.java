51408befc227e3790e6e84b8c639e07012826949
MOHAMMED ALHAMRASHDI <Malhamrashdi@gmail.com>
2026-08-17 10:37:30 +0400

    Person

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,4 +1,15 @@
 package entities;
-
+import interfaces.Displayable;
 public class Person {
+    public  class persom implements Displayable{
+        private String id;
+        private String firstName;
+        private String LastName;
+        private String dateofBirthday;
+        private String gender;
+        private String phoneNumber;
+        private String email;
+        private String address;
+
+    }
 }
