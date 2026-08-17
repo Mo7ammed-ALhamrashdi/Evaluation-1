462557d82bcf65d721365db8c707ad35019e0d78
MOHAMMED ALHAMRASHDI <Malhamrashdi@gmail.com>
2026-08-17 14:49:29 +0400

    Searchable

--- src/interfaces/Searchable.java
+++ src/interfaces/Searchable.java
@@ -1,4 +1,10 @@
 package interfaces;
 
-public interface Searchable {
+import java.util.List;
+
+public interface Searchable<T> {
+
+    List<T> search(String keyword);
+
+    T searchById(String id);
 }
