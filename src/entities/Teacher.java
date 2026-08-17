2ef83e12b6996f4c10680f8533e8c6a8efb477ef
MOHAMMED ALHAMRASHDI <Malhamrashdi@gmail.com>
2026-08-17 14:15:11 +0400

    Teacher

--- src/entities/Teacher.java
+++ src/entities/Teacher.java
@@ -1,4 +1,30 @@
 package entities;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Teacher {
-}
+    public class Teacher extends Person {
+
+        private String subject;
+        private int experienceYears;
+        private double salary;
+        private List<String> classesTaught;
+
+        public Teacher(String id, String firstName, String lastName,
+                       String dateOfBirth, String gender,
+                       String phoneNumber, String email, String address,
+                       String subject, int experienceYears,
+                       double salary) {
+
+            super(id, firstName, lastName,
+                    dateOfBirth, gender,
+                    phoneNumber, email, address);
+
+            this.subject = subject;
+
+            setExperienceYears(experienceYears);
+
+            this.salary = salary;
+
+            this.classesTaught = new ArrayList<>();
+        }
+    }}
