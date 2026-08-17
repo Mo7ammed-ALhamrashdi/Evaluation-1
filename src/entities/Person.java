aff779ecdb519428f7eac32eac21340bd8c30d70
MOHAMMED ALHAMRASHDI <Malhamrashdi@gmail.com>
2026-08-17 11:15:32 +0400

    Person

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -1,16 +1,116 @@
 package entities;
-import interfaces.Displayable;
-    public  class Person implements Displayable{
-        private String id;
-        private String firstName;
-        private String LastName;
-        private String dateofBirthday;
-        private String gender;
-        private String phoneNumber;
-        private String email;
-        private String address;
-public Person (String id, String firstName, String lastName, String dateOfBirth,
-              String gender, String phoneNumber, String email, String address){
 
-}
+import interfaces.Displayable;
+
+public class Person implements Displayable {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    public Person(String id, String firstName, String lastName,
+                  String dateOfBirth, String gender,
+                  String phoneNumber, String email, String address) {
+
+        setId(id);
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
     }
+    // Getters
+    public String getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    // Setters
+    public void setId(String id) {
+        if (utils.HelperUtils.isEmpty(id)) {
+            System.out.println("Id cannot be empty. Keeping the old id.");
+            return;
+        }
+        this.id = id;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    // Overriding from Displayable
+    @Override
+    public void displayInfo() {
+        System.out.println(
+                "Person: " + firstName + " " + lastName +
+                        " (id: " + id + ")"
+        );
+    }
+    @Override
+    public String displaySummary() {
+        return id + " - " + firstName + " " + lastName;
+    }
+    @Override
+    public String toString() {
+        return "Person{id=" + id +
+                ", name=" + firstName + " " + lastName + "}";
+    }
+    // Compare persons using ID
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (!(other instanceof Person)) {
+            return false;
+        }
+        Person p = (Person) other;
+        return this.id.equals(p.id);
+    }
+}
