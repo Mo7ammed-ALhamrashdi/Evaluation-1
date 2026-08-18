c3e8b991c7133fa9852313df7b66a04ba1ac4485
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-08-18 12:18:32 +0400

    Person

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -13,9 +13,9 @@ public class Person implements Displayable {
     private String email;
     private String address;
 
-    public Person(String id, String firstName, String lastName,
+    public Person(String id, String firstName , String lastName,
                   String dateOfBirth, String gender,
-                  String phoneNumber, String email, String address) {
+                  String phoneNumber , String email, String address) {
 
         setId(id);
 
